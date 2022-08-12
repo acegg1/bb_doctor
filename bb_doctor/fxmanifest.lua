a94c6ba54a6da5f5d36ffd6ab286c60c6bda7b50
BlackBear <111135227+BllackBear@users.noreply.github.com>
2022-08-12 16:28:35 +0200

    Add files via upload

---
+++ bb_doctor/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "bb_doctor"
+description "Heals And Gives Armour To Players"
+author "BlackBear"
+version "1.0.0"
+
+config_scripts {
+	'config/*.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
